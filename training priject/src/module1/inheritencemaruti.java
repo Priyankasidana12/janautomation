733801cb15de274d0740b8f5ace57718375994e8
HP <HP@DESKTOP-F3LUCSS>
2024-02-23 13:24:00 +1100

    rest assured

---
+++ training priject/src/module1/inheritencemaruti.java
@@ -0,0 +1,13 @@
+package module1;
+
+public class inheritencemaruti {
+	
+	public void method1() {
+		System.out.println("this is parent classmethod 1");
+		
+	}
+	public void method2() {
+		System.out.println("parent class method2" );
+		
+	}
+}
---
+++ training priject/src/module1/inheritenceswift.java
@@ -0,0 +1,23 @@
+package module1;
+
+public class inheritenceswift extends inheritencemaruti{
+
+	public void method3() {
+		System.out.println("this is child method 3");
+	}
+	
+	public void method4() {
+		System.out.println("this is child method4");
+		
+	}
+	public static void main(String[] args) {
+		inheritenceswift s= new inheritenceswift();
+		s.method1();
+		s.method2();
+		s.method3();
+		s.method4();
+		s.abc=123;
+		System.out.println("the value of abc");
+	}
+}
+	
